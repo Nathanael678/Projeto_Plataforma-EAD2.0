32dfd20ed2ea5aace59dd1c3276a5d480f8ccca7
NATHANAEL ALVES RODRIGUES <nathanaelalves786@gmail.com>
2026-05-29 20:09:39 -0300

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router  = express.Router();
-
-router.post('/login', (req, res) => {
-  res.json({ message: "Rota temporária" });
-});
-
-module.exports = router;
